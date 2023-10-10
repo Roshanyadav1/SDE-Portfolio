37385cf2d211192176825eb474cc649a0fcefaf5
roshan <roshany.bca2020@ssism.org>
2023-10-10 16:11:25 +0530

    remove extra config

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/homepage";
 import About from "./pages/about";
 import Projects from "./pages/projects";
-// import Articles from "./pages/articles";
-import ReadArticle from "./pages/readArticle";
 import Contact from "./pages/contact";
 import Notfound from "./pages/404";
 
@@ -20,8 +18,6 @@ function App() {
 					<Route path="/" element={<Homepage />} />
 					<Route path="/about" element={<About />} />
 					<Route path="/projects" element={<Projects />} />
-					{/* <Route path="/articles" element={<Articles />} /> */}
-					<Route path="/article/:slug" element={<ReadArticle />} />
 					<Route path="/contact" element={<Contact />} />
 					<Route path="*" element={<Notfound />} />
 				</Routes>
